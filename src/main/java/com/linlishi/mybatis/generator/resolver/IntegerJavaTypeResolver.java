e33c7bc9cffd344be807060d1f0556923249ea14
linlishi <lishi.lin@urbanic.com>
2024-01-24 11:51:17 +0800

    tinyint和smallint 映射成java integer

---
+++ src/main/java/com/linlishi/mybatis/generator/resolver/IntegerJavaTypeResolver.java
@@ -0,0 +1,20 @@
+package com.linlishi.mybatis.generator.resolver;
+
+import org.mybatis.generator.api.dom.java.FullyQualifiedJavaType;
+import org.mybatis.generator.internal.types.JavaTypeResolverDefaultImpl;
+
+import java.sql.Types;
+
+/**
+ * @author chutou
+ * @date 2024/1/24 11:43
+ **/
+public class IntegerJavaTypeResolver extends JavaTypeResolverDefaultImpl {
+    public IntegerJavaTypeResolver() {
+        super();
+        // tinyint 映射到java integer
+        this.typeMap.put(Types.TINYINT, new JdbcTypeInformation("INTEGER", new FullyQualifiedJavaType(Integer.class.getName())));
+        // smallint 映射到java integer
+        this.typeMap.put(Types.SMALLINT, new JdbcTypeInformation("INTEGER", new FullyQualifiedJavaType(Integer.class.getName())));
+    }
+}
